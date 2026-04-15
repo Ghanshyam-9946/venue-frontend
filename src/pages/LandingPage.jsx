f70b79a190606bb3e3a454327b4be3725838e4cd
Ghanshyam-9946 <dhoteghanshyam9@gmail.com>
2026-04-15 14:27:12 +0530

    link

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -347,17 +347,17 @@ const Footer = () => {
             <ul className="space-y-4">
 
               <li className="group">
-                <a href="#" className="hover:text-pink-500 transition-colors text-sm text-slate-400 flex items-center gap-2">
+                <a href="https://www.instagram.com/sagarcollegebhopal/" className="hover:text-pink-500 transition-colors text-sm text-slate-400 flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4 text-slate-500 group-hover:text-pink-500 transition-colors"><rect width="20" height="20" x="2" y="2" rx="5" ry="5" /><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" /><line x1="17.5" x2="17.51" y1="6.5" y2="6.5" /></svg> Instagram
                 </a>
               </li>
               <li className="group">
-                <a href="#" className="hover:text-blue-500 transition-colors text-sm text-slate-400 flex items-center gap-2">
+                <a href="https://www.linkedin.com/school/sagar-group-of-institutions-sistec/posts/?feedView=all" className="hover:text-blue-500 transition-colors text-sm text-slate-400 flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4 text-slate-500 group-hover:text-blue-500 transition-colors"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect width="4" height="12" x="2" y="9" /><circle cx="4" cy="4" r="2" /></svg> LinkedIn
                 </a>
               </li>
               <li className="group">
-                <a href="#" className="hover:text-slate-100 transition-colors text-sm text-slate-400 flex items-center gap-2">
+                <a href="https://x.com/sagarcollege" className="hover:text-slate-100 transition-colors text-sm text-slate-400 flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="none" className="w-4 h-4 text-slate-500 group-hover:text-slate-100 transition-colors"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" /></svg> Twitter
                 </a>
               </li>
